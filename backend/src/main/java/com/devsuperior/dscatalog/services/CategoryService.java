b3c78c16841b8009e7a4db0fe4529187c1971e05
Daniel Xavier <dxavier194@gmail.com>
2022-01-19 20:01:12 -0300

    Delete category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -24,11 +27,9 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 
 		/*
-		 * Esse bloco foi feito em apenas uma linha utilizando o map. 
-		 * List<CategoryDTO> listDto = new ArrayList<>(); 
-		 * for (Category x : list) { 
-		 * listDto.add(new CategoryDTO(x));
-		 *  }
+		 * Esse bloco foi feito em apenas uma linha utilizando o map. List<CategoryDTO>
+		 * listDto = new ArrayList<>(); for (Category x : list) { listDto.add(new
+		 * CategoryDTO(x)); }
 		 */
 
 		return list.stream().map(x -> new CategoryDTO(x)).toList();
@@ -37,7 +38,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Category entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-		
+
 		return new CategoryDTO(entity);
 	}
 
@@ -56,9 +57,18 @@ public class CategoryService {
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
-		}
-		catch (EntityNotFoundException e) {
+		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
