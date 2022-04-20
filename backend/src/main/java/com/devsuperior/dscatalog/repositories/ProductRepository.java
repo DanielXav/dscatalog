b9d5fcbbfc6702b58d30372a5cb57d50ba50061c
Daniel Xavier <dxavier194@gmail.com>
2022-04-20 14:56:21 -0300

    Filter by name

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -13,6 +13,7 @@ import com.devsuperior.dscatalog.entities.Product;
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
-			+ "(:category IS NULL OR :category IN cats)")
-	Page<Product> find(Category category, Pageable pageable);
+			+ "(:category IS NULL OR :category IN cats) AND "
+			+ "(LOWER(obj.name) LIKE LOWER(CONCAT('%', :name, '%')) )")
+	Page<Product> find(Category category, String name, Pageable pageable);
 }
